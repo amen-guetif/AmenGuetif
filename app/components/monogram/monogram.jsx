a2dde29ebb8fa9d43b30da4e7d5af5145ec2718a
amenguetif <amenguetif3@gmail.com>
2024-11-15 10:47:11 +0100

    Deploy React app

---
+++ app/components/monogram/monogram.jsx
@@ -0,0 +1,32 @@
+import { forwardRef, useId } from 'react';
+import { classes } from '~/utils/style';
+import styles from './monogram.module.css';
+
+export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => {
+  const id = useId();
+  const clipId = `${id}monogram-clip`;
+
+  return (
+    <svg
+      aria-hidden
+      className={classes(styles.monogram, className)}
+      width="48"
+      height="29"
+      viewBox="0 0 48 29"
+      ref={ref}
+      {...props}
+    >
+      <defs>
+        <clipPath id={clipId}>
+          <path d="M24 0L12 29h6l2.5-6h13L30 29h6L24 0Zm-2.5 17L24 6.5 26.5 17h-5Z" />
+        </clipPath>
+      </defs>
+      <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
+      {highlight && (
+        <g clipPath={`url(#${clipId})`}>
+          <rect className={styles.highlight} width="100%" height="100%" />
+        </g>
+      )}
+    </svg>
+  );
+});
---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,9 @@
+import config from '~/config.json';
+
+export const navLinks = [
+ 
+];
+
+export const socialLinks = [
+  
+];
